f0db6c34aafd882522580f2d332475942389c794
Umer Farooq <umerf6711@gmail.com>
2023-10-19 12:14:08 +0500

    exception and logger

--- src/exception.py
+++ src/exception.py
@@ -19,3 +19,13 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message    
+    
+    
+    
+if __name__=="__main__":
+    
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info('Zero division error') 
+        raise CustomException(e,sys)       
